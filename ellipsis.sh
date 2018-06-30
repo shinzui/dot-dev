718668d65801793174f75433ac80e36e6fdadbe5
Nadeem Bitar <nadeem@gmail.com>
2018-06-30 10:00:02 -0700

    :package: Add fonts

--- ellipsis.sh
+++ ellipsis.sh
@@ -5,6 +5,9 @@
 # The following hooks can be defined to customize behavior of your package:
 pkg.install() {
   brew install watchexec just
+  brew tap caskroom/fonts
+  brew cask install font-iosevka
+  brew cask install font-fira-code
 }
 
 # pkg.push() {
