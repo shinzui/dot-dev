cd197aa661f7428a50b758184b050ca90831fda5
Nadeem Bitar <nadeem@gmail.com>
2018-10-07 09:43:14 -0700

    :scroll: Add Nord theme to bat

--- ellipsis.sh
+++ ellipsis.sh
@@ -2,13 +2,32 @@
 #
 # shinzui/dev ellipsis package
 
+configure_bat() {
+  BAT_CONFIG_DIR="$(bat cache --config-dir)"
+
+  mkdir -p "$BAT_CONFIG_DIR/themes"
+
+  cp Nord.tmTheme "$BAT_CONFIG_DIR/themes"
+
+  bat cache --init
+
+}
+
 # The following hooks can be defined to customize behavior of your package:
 pkg.install() {
-  brew install watchexec just
+  brew install watchexec just bat fd
   brew tap caskroom/fonts
   brew cask install font-iosevka
   brew cask install font-fira-code
+
+  configure_bat
 }
+
+pkg.link() {
+  #do nothing
+  echo ""
+}
+
 
 # pkg.push() {
 #     git.push
